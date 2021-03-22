cb1cae3c6ca38d81d1f63ce9a8f40ec12e608011
bsumanji <layzeesuman1993@gmail.com>
2021-03-22 22:50:37 +0545

    This is my third commit where i have added gradient

--- Screens/welcomeScreen.js
+++ Screens/welcomeScreen.js
@@ -2,14 +2,20 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Button, Input, Image } from 'react-native-elements';
 import homeScreen from './homeScreen';
-
-
+//import LinearGradient from 'react-native-linear-gradient'
+import { LinearGradient } from 'expo-linear-gradient';
 
 const welcomeScreen = ({ navigation }) => {
     return (
       <View style={styles.container}>
-      
-      <Image
+      <LinearGradient
+        // Background Linear Gradient
+        colors={["#48F10E", "#078716", "#093203","yellow","red", 'transparent']}
+        style={styles.background}
+        start={{x:0,y:0}}
+        end={{x:1,y:1}}
+      />
+     <Image
         style={{width:300, height:100}}
         source={{
           uri: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
@@ -17,12 +23,9 @@ const welcomeScreen = ({ navigation }) => {
         onPress={() => navigation.navigate("Home")}
 
       />
-      
-    </View>
 
-      
-        
-    )
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -32,7 +35,16 @@ const styles = StyleSheet.create({
     justifyContent:'center',
     alignItems: 'center',
     
-  }
+  },
+
+  background:{
+    position:'absolute',
+
+    left:0,
+    right:0,
+    height:1000,
+
+  },
   
 });
 	
